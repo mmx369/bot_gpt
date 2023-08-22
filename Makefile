c712a10d64b608b357a329117d365650a80634cc
mmx369 <maksim@klishevich.com>
2023-08-22 22:54:33 +0300

    add docker

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	docker build -t gpt-bot .
+
+run:
+	docker run -d -p 3000:3000 --name gpt-bot --rm gpt-bot
+
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import config from 'config'
-import { Telegraf } from 'telegraf'
+import { Telegraf, session } from 'telegraf'
 import { code } from 'telegraf/format'
 import { ogg } from './ogg.js'
 import { openai } from './openai.js'
 
+console.log(config.get('TEST_ENV'))
+
+const INITIAL_SESSION = {
+  messages: [],
+}
+
 const bot = new Telegraf(config.get('TELRGRAM_TOKEN'))
 
-bot.launch()
+bot.use(session())
+
+bot.command('new', async (ctx) => {
+  //@ts-ignore
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Waiting for your messages...')
+})
+
+bot.command('start', async (ctx) => {
+  //@ts-ignore
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Waiting for your messages...')
+})
 
 bot.on('voice', async (ctx) => {
+  //@ts-ignore
+  ctx.session ??= INITIAL_SESSION
+  //@ts-ignore
   try {
     await ctx.reply(code('Waiting response from server...'))
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
@@ -18,9 +40,15 @@ bot.on('voice', async (ctx) => {
 
     const text = (await openai.transcription(mp3Path)) as string
     await ctx.reply(code(text))
-    const messages = [{ role: openai.roles.USER, content: text }]
     //@ts-ignore
-    const response = await openai.chat(messages)
+    ctx.session.messages.push({ role: openai.roles.USER, content: text })
+    //@ts-ignore
+    const response = await openai.chat(ctx.session.messages)
+    //@ts-ignore
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response?.content,
+    })
     //@ts-ignore
     await ctx.reply(response?.content)
   } catch (err) {
@@ -32,9 +60,36 @@ bot.on('voice', async (ctx) => {
   }
 })
 
-bot.command('start', async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx.message, null, 2))
+bot.on('text', async (ctx) => {
+  //@ts-ignore
+  ctx.session ??= INITIAL_SESSION
+  //@ts-ignore
+  try {
+    await ctx.reply(code('Waiting response from server...'))
+    //@ts-ignore
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    })
+    //@ts-ignore
+    const response = await openai.chat(ctx.session.messages)
+    //@ts-ignore
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response?.content,
+    })
+    //@ts-ignore
+    await ctx.reply(response?.content)
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log('Error: ', err.message)
+    } else {
+      console.log('Unexpected error', err)
+    }
+  }
 })
+
+bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
